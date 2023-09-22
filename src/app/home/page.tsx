1c35251df91e4ee21ee402cf4d0dcda57cbf1086
jovan <jvaonam@me.com>
2023-09-21 23:22:56 -0600

    feat: initial config

---
+++ src/app/home/page.tsx
@@ -0,0 +1,12 @@
+import React, { ReactElement } from "react";
+
+const HomePage = (): ReactElement => {
+  return (
+    <div>
+      <h1>Welcome to our E-commerce store!</h1>
+      <p>Check out our latest products and deals.</p>
+    </div>
+  );
+};
+
+export default HomePage;
